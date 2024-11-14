6b4caed0f4d1e93eb774a1449486ea560d72a047
mzackrisson <80635449+mzackrisson@users.noreply.github.com>
2024-11-14 16:46:41 +0100

    move infoblock to hero, layer infoblcoks on top of hero on large screens, add transparent gray on top of hero to make text easier to read, remove infoblock component

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
-
+import Icons from "unplugin-icons/vite";
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro";
@@ -11,15 +11,22 @@ import keystatic from "@keystatic/astro";
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
+  vite: {
+    plugins: [
+      Icons({
+        compiler: "astro",
+      }),
+    ],
+  },
   integrations: [
     tailwind(),
     mdx(),
     sitemap(),
     icon(),
+    Icons(),
     react(),
     markdoc(),
-    keystatic()
+    keystatic(),
   ],
   output: "hybrid",
 });
-
