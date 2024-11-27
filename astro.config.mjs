78f414de508c2347e4b9fea1a8e74e3dff8d8ee2
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2024-11-27 20:14:00 +0100

    Remove unused deps and simplify project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,21 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import icon from "astro-icon";
-import Icons from "unplugin-icons/vite";
-import react from "@astrojs/react";
-import markdoc from "@astrojs/markdoc";
-import keystatic from "@keystatic/astro";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import Icons from 'unplugin-icons/vite'
+import markdoc from '@astrojs/markdoc'
+import keystatic from '@keystatic/astro'
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: 'https://brfbonneville.se',
   vite: {
     plugins: [
       Icons({
-        compiler: "astro",
+        compiler: 'astro',
       }),
     ],
   },
-  integrations: [
-    tailwind(),
-    mdx(),
-    sitemap(),
-    icon(),
-    Icons(),
-    react(),
-    markdoc(),
-    keystatic(),
-  ],
-  output: "hybrid",
-});
+  integrations: [tailwind(), mdx(), sitemap(), icon(), markdoc(), keystatic()],
+  output: 'hybrid',
+})
