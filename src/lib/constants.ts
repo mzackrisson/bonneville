eb124c7c6b231e51483c03985af6c779994bf1a3
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2024-11-27 22:54:02 +0100

    Keep constants in one place

---
+++ src/lib/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_NAME = 'BRF Bonneville'
+export const SITE_DESCRIPTION =
+  'BRF Bonneville är en bostadsrättsförening i Vega, Haninge som har 45 lägenheter fördelade på två hus.'
+export const BASE_TITLE = 'BRF Bonneville i Vega'
